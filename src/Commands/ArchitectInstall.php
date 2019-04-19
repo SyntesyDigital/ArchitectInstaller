28fa220049768b4f61d4740ef26a0f0db12600c0
SyntesyDigital <ndelcast.dev@gmail.com>
2019-04-19 13:21:30 +0200

    Add dependencies

--- src/Commands/ArchitectInstall.php
+++ src/Commands/ArchitectInstall.php
@@ -16,21 +16,29 @@ class ArchitectInstall extends Command
             'url' => 'https://github.com/SyntesyDigital/ArchitectCore',
             'directory' => 'Architect',
             'vendors' => [
-                "acoustep/entrust-gui",
                 "barryvdh/laravel-cors",
-                "barryvdh/laravel-debugbar",
                 "doctrine/dbal",
                 "intervention/image",
                 "jenssegers/date",
                 "kalnoy/nestedset",
                 "laravelcollective/html",
-                "predis/predis",
                 "prettus/l5-repository",
-                "prettus/laravel-validation",
-                "toin0u/geocoder-laravel",
-                "tymon/jwt-auth",
                 "yajra/laravel-datatables-oracle",
                 "zizaco/entrust",
+            ],
+            'providers' => [
+                'Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider::class',
+                'Zizaco\Entrust\EntrustServiceProvider::class',
+                'Yajra\DataTables\DataTablesServiceProvider::class',
+                'Mariuzzo\LaravelJsLocalization\LaravelJsLocalizationServiceProvider::class',
+            ],
+            'facades' => [
+                'LaravelLocalization' => 'Mcamara\LaravelLocalization\Facades\LaravelLocalization::class',
+                'Entrust' => 'Zizaco\Entrust\EntrustFacade::class',
+                'Datatables' => 'Yajra\DataTables\Facades\DataTables::class',
+            ],
+            'webpack' => [
+
             ]
         ],
 
@@ -128,15 +136,34 @@ class ArchitectInstall extends Command
             return false;
         }
 
+        $this->installDependencies($package);
 
+        $this->installPackage($package);
+
+        // Dump autoload
+        $this->info('--- (2/4) DUMP AUTOLOAD ----');
+        exec('composer dumpautoload');
+        $this->info('... DONE');
+
+        // Migrate module DB
+        $this->info('--- (3/3) MIGRATE MODULE DB ----');
+        exec('php artisan module:migrate ' . $package["directory"]);
+        $this->info('... DONE');
+    }
+
+
+    private function installPackage($package)
+    {
         // Clone Module directory
         $this->info('--- (1/4) CLONING REPOSITORY ----');
         exec("git clone ".$package["url"]."  Modules/" . $package["directory"]);
         $this->info('... DONE');
+    }
 
-
+    private function installDependencies($package)
+    {
         // Install dependencies
-        $this->info('--- (3/4) INSTALLING DEPENDENCIES ----');
+        $this->info('--- (3/4) INSTALLING VENDORS ----');
         if(isset($package["vendors"])) {
             foreach($package["vendors"] as $vendor) {
                 $this->info("... INSTALLING $vendor");
@@ -146,18 +173,6 @@ class ArchitectInstall extends Command
         } else {
             $this->info('... NOTHING TO INSTALL');
         }
-
-
-        // Dump autoload
-        $this->info('--- (2/4) DUMP AUTOLOAD ----');
-        exec('composer dumpautoload');
-        $this->info('... DONE');
-
-
-        // Migrate module DB
-        $this->info('--- (3/3) MIGRATE MODULE DB ----');
-        exec('php artisan module:migrate ' . $package["directory"]);
-        $this->info('... DONE');
     }
 
 
